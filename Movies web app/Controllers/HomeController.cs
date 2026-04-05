78dc54b40c7c07188027092943c1a9f8481fe7d1
Refa3y.dev-dotNet <refa3y.dev@gmail.com>
2026-04-05 23:56:35 +0200

    Inject IMovieManager and async movie fetch in HomeController
    Refactored HomeController to inject IMovieManager via constructor. Updated Index action to be asynchronous and fetch movies using _movieManager.GetAllMoviesAsync(). Added necessary using directives for dependencies.

--- Movies web app/Controllers/HomeController.cs
+++ Movies web app/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Business.Managers.Movies;
 using Microsoft.AspNetCore.Mvc;
 using Movies_web_app.Models;
 using System.Diagnostics;
@@ -7,14 +8,18 @@ namespace Movies_web_app.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IMovieManager _movieManager;
 
-    public HomeController(ILogger<HomeController> logger)
+
+    public HomeController(ILogger<HomeController> logger, IMovieManager movieManager)
     {
         _logger = logger;
+        _movieManager = movieManager;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        var activeMovires=await _movieManager.GetAllMoviesAsync();
         return View();
     }
 
